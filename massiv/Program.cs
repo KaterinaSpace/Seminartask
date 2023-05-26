bb438949dbef2e211dd76d3b9c88796714fb88b7
Ekaterina Goltseva <Katarinka98@inbox.ru>
2023-05-26 16:39:16 +0300

    add  massiv

---
+++ massiv/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[4];
+array[0] = 1;
+array[1] = 2;
+array[2] = 3;
+array[3] = 4;
+ 
+Console.WriteLine(array.Length);
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
