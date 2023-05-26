419718528438393ae8dc357a0ba3866ce9b2443f
Ekaterina Goltseva <Katarinka98@inbox.ru>
2023-05-27 01:20:53 +0300

    something

---
+++ task_30var2/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random().Next(0, 2);
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count - 1)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+    Console.Write($"{col[position]}");
+}
+
+Console.Clear();
+
+int[] array = new int[100];
+FillArray(array);
+PrintArray(array);
