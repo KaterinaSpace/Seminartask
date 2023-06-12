3a0fbd537fd17de4220b7a4686bbc6fd31be8393
Katerina <Katarinka98@inbox.ru>
2023-06-12 18:12:22 +0300

    add homeworktask 25

--- taskhomework_25/Program.cs
+++ taskhomework_25/Program.cs
@@ -2,4 +2,26 @@
 //  и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+Console.WriteLine("Введите первое число A");
+int numA = GetInputNumber();
+Console.WriteLine("Введите второе число B");
+int numB = GetInputNumber();
+ToDegree(numA, numB);
 
+
+int GetInputNumber()
+{
+  string input = Console.ReadLine();
+  return int.Parse(input);
+}
+
+void ToDegree(int a, int b)
+{
+  int result = 1;
+  for (int i = 1; i <= b; i++)
+  {
+    result = result * a;
+
+  }
+  Console.WriteLine(result);
+}
