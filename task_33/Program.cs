e46b98ff89c7fce38c3a86c19c217e11bc3ec99c
Katerina <Katarinka98@inbox.ru>
2023-06-23 16:35:12 +0300

    add task 33

---
+++ task_33/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+
+int[] array = { 6, 7, 19, 345, 3 };
+Console.WriteLine("Введите число, котрое надо найти");
+int number = ReadNumberFromConsole();
+bool result = FindNumberInArray(array, number);
+if (result == true)
+{
+  Console.WriteLine("Да");
+}
+else
+{
+  Console.WriteLine("Нет");
+}
+
+
+bool FindNumberInArray(int[] array, int numberToSearch)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] == numberToSearch)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+void PrintArray(int[] array)
+{
+  Console.Write("{");
+  for (int i = 0; i < array.Length - 1; i++)
+  {
+    Console.Write($"{array[i]},");
+  }
+  Console.WriteLine(array[^1] + "}");
+}
+
+int ReadNumberFromConsole()
+{
+  string input = Console.ReadLine();
+  return int.Parse(input);
+}
