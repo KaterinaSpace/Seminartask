d98f59d96b699dc3182d5f3ef1f4f56d11a041c4
Katerina <Katarinka98@inbox.ru>
2023-06-23 13:31:11 +0300

    add task 32

--- task_32/Program.cs
+++ task_32/Program.cs
@@ -1,15 +1,36 @@
 ﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
-// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
-// 4; массив [6, 7, 19, 345, 3] -> нет
-// 3; массив [6, 7, 19, 345, 3] -> да
-
-int[] arra = new int[4];
-Array[1] = -4;
-Array[2] = -8;
-Array[3] = -8;
-Array[4] = -2;
 
 
+// int[] array = new int[4];
+// Array[1] = -4;
+// Array[2] = -8;
+// Array[3] = -8;
+// Array[4] = -2;
 
-  
+int[] array = { -4, -8, 8, 2 };
+PrintArray(array);
+NewArray(array);
+PrintArray(array);
+
+void NewArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = array[i] * -1;
+  }
+}
+
+void PrintArray(int[] array)
+{
+  Console.Write("{");
+  for (int i = 0; i < array.Length - 1; i++)
+  {
+    Console.Write($"{array[i]},");
+  }
+  Console.WriteLine(array[^1] + "}");
+}
+
+
+
+
