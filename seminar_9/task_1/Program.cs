2f750c7e66e5ca7b3ed4507267798ec70bafaa38
Katerina <Katarinka98@inbox.ru>
2023-09-11 23:35:05 +0300

    add task 1 recursion

---
+++ seminar_9/task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿//напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма цифр в числе {number} - {SumNumbers(number)}");
+
+int SumNumbers(int numbers){
+if(numbers == 0) return 0;
+return (numbers % 10 + SumNumbers(numbers / 10));
+}
---
+++ task_52/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] GetArray(int m, int n)
+
+{
+	int[,] matrix = new int[m, n];
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		
+		{
+			matrix[i, j] = new Random().Next(1, 10);
+		}
+	}
+	return matrix; 
+}
+
+void PrintArray(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		
+		{
+			Console.Write($"{matrix[i, j], 4}");
+		}
+		Console.WriteLine();
+	}
+}
+
+double SimpleAverageRows (int[,] matrix)
+
+{
+	int simpleA
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	
+	{
+		for (int j = 0; j < matrix.GetLe(1); j++)
+		{
+
+		}
+	}
+}
+
+
+
+int[,] matrix = GetArray(5, 5);
+PrintArray(matrix);
