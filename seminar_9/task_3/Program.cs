e7f419873fb5f3d74ba10fc46b9a709185a280cb
Katerina <Katarinka98@inbox.ru>
2023-09-16 15:34:45 +0300

    add task 3

--- seminar_9/task_3/Program.cs
+++ seminar_9/task_3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2:** Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M и N.
+
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8; -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+string PrintNumbers(int m, int n)
+{
+    if (m == n)
+        return m.ToString(); //почему тустринг
+    return (m + ", " + PrintNumbers(m + 1, n));
+}
