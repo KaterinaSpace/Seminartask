7818635472276938603d2c216c9f3a9b9c56ab17
Katerina <Katarinka98@inbox.ru>
2023-09-17 19:24:12 +0300

    add task the towers

--- Lesson_7/Program.cs
+++ Lesson_7/Program.cs
@@ -1,5 +1,11 @@
-﻿//Собрать строку с числами от a до b, a<=b
+﻿//towers
+void Towers(string with = "1", string on = "3", string some = "2", int count = 4)
+{
+    if (count > 1)
+        Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1)
+        Towers(some, on, with, count - 1);
+}
 
-//итеративный подход
-
-string NumbersFor(int a, int b)
+Towers();
